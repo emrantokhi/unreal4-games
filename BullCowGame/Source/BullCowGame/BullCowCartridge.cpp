fcfece7404b085baccb195f7d4e752cd890c6f2b
Emran Tokhi <emrantokhiwork@gmail.com>
2021-10-14 10:37:33 -0700

    Allow guesses to be inputted by player

--- BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,12 +5,20 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine(TEXT("Welcome to Bull Cows!"));
-    PrintLine(TEXT("Input any character(s) and press enter!"));
+    PrintLine(TEXT("Guess the 5 letter word!")); //Hard coded number, Remove it
+    PrintLine(TEXT("Type in a guess!"));
+    PrintLine(TEXT("Press enter to continue!"));
 
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = TEXT("muted");
+    FString HiddenWord = TEXT("muted"); //move this outside of function so it doesnt keep being recalled
+    if (Input == HiddenWord) { // == is case insensitive, .Equals is case sensitive
+        PrintLine(Input + TEXT(" is the hidden word!"));
+    }
+    else {
+        PrintLine(Input + TEXT(" is not the hidden word. Try again!"));
+    }
 }
